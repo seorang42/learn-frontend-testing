5eddd1a4953a574f7a46fc7341e5dc7b52cc03fd
seorang42 <seorang42@naver.com>
2025-04-18 20:31:56 +0900

    2-4 jest 실패 케이스 작성

--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import SignupPage from "../pages/SignupPage";
 import { createMemoryRouter } from "react-router";
 import { RouterProvider } from "react-router-dom";
@@ -10,8 +10,9 @@ const queryClient = new QueryClient({
 });
 
 describe("회원가입 테스트", () => {
-  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다", () => {
+  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다", async () => {
     // given - 회원가입 페이지가 그려짐
+
     const routes = [
       {
         path: "/signup",
@@ -31,6 +32,15 @@ describe("회원가입 테스트", () => {
     );
 
     // when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
+
+    const passwordInput = screen.getByLabelText("비밀번호");
+    const confirmInput = screen.getByLabelText("비밀번호 확인");
+
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.change(confirmInput, { target: { value: "wrongPassword" } });
+
     // then - 에러 메세지가 표시됨
+    const errorMessage = await screen.findByTestId("error-message");
+    expect(errorMessage).toBeInTheDocument();
   });
 });
