599467e741817c68720aa95be1944d3736ddf85e
seorang42 <seorang42@naver.com>
2025-04-19 20:16:18 +0900

    3-9 recoil을 테스트하는 방법

--- cypress/e2e/2-order/order.cy.ts
+++ cypress/e2e/2-order/order.cy.ts
@@ -93,4 +93,34 @@ describe("주문을 테스트 한다", () => {
       cy.url().should("include", "/restaurant/1");
     });
   });
+
+  it("사용자는 원하는 메뉴를 장바구니에 담고, 원하는 음식 개수를 선택할 수 있다", () => {
+    cy.visit("/restaurant/1");
+    cy.intercept(
+      {
+        method: "GET",
+        url: "/restaurant/1",
+      },
+      {
+        fixture: "menu.json",
+      }
+    );
+
+    cy.fixture("menu.json").then((menu) => {
+      cy.get(`[data-cy=${menu.menu_set[0].id}]`)
+        .should("be.visible")
+        .as("foodBtn");
+      cy.get("@foodBtn").click();
+
+      cy.url().should("include", "/order");
+      cy.get("[data-cy=counter]").as("counter");
+      cy.get("@counter").should("contain", 1);
+      cy.get("[data-cy=incrementBtn]").should("be.visible").click();
+      cy.get("@counter").should("contain", 2);
+      cy.get("[data-cy=decrementBtn]").should("be.visible").click();
+      cy.get("@counter").should("contain", 1);
+      cy.get("[data-cy=completeBtn]").should("be.visible").click();
+      cy.url().should("include", "/");
+    });
+  });
 });
