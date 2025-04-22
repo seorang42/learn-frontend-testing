21ba5cd0e92d9ad133e68c9561d5e2d7228909b8
seorang42 <seorang42@naver.com>
2025-04-22 23:01:12 +0900

    빌드 오류 해결

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,5 @@ export default defineConfig({
   projectId: "bx35wo",
   e2e: {
     baseUrl: "http://localhost:5173",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
